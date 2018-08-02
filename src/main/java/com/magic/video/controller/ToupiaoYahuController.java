25f6e2f5916a6d5da81e94084c397fb302fd52e5
admin <admin@qianjunmin>
2018-08-02 10:07:02 +0800

    1

--- src/main/java/com/magic/video/controller/ToupiaoYahuController.java
+++ src/main/java/com/magic/video/controller/ToupiaoYahuController.java
@@ -1,6 +1,7 @@
 package com.magic.video.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Random;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -24,22 +25,44 @@ public class ToupiaoYahuController extends CustomController{
 	@PostMapping(value="zjs")
 	public void zjs() throws InterruptedException, UnsupportedEncodingException {
 		runLog.info("雅虎投票.");
-
-		for(int i = 0;i<1400;i++) {
-			System.out.println("循环次数："+i);
-    		String url = "http://f1ol1l.jia.yahuq.top/app/index.php?i=32&c=entry&rid=3353&id=254626&do=vote&m=tyzm_diamondvote";
-    		HttpHeaders headers = new HttpHeaders();
-            headers.set("X-Requested-With", "XMLHttpRequest");
-            headers.set("User-Agent", "Mozilla/5.0 (Linux; Android 7.1.1; OD103 Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044203 Mobile Safari/537.36 MicroMessenger/6.6.6.1300(0x26060632) NetType/WIFI Language/zh_CN");
-    		String result = HttpClient.clientHeader(url, HttpMethod.GET, null, headers);
-    		System.out.println("result="+result);
-    		JSONObject obj=JSON.parseObject(result);
-    		String msg = obj.get("msg").toString();
-    		System.out.println("msg="+msg);
-			int random=(int) (Math.random()*1000+2000);
-			System.out.println("随机时间："+random);
-			Thread.sleep(random);
-    	}
+		
+		for(int i = 0;i<20;i++) {
+			Thread iThread = new ZjsThread();
+			iThread.start();
+		}
+		
+	}
+	
+	private class ZjsThread extends Thread{
+        @Override
+        public void run() {
+        	System.out.println("当前线程："+Thread.currentThread().getName()+",start");
+        	//循环请求100次
+        	for(int i = 0;i<10000;i++) {
+        		System.out.println("循环次数："+i);
+    			//http://f1ol1l.jia.yahuq.top/app/index.php?i=32&c=entry&rid=3353&id=254609&do=vote&m=tyzm_diamondvote
+    			//http://f1ol1l.jia.yahuq.top/app/index.php?i=32&c=entry&rid=3353&id=254626&do=vote&m=tyzm_diamondvote
+        		String url = "http://f1ol1l.jia.yahuq.top/app/index.php?i=32&c=entry&rid=3353&id=254609&do=vote&m=tyzm_diamondvote";
+        		HttpHeaders headers = new HttpHeaders();
+                headers.set("X-Requested-With", "XMLHttpRequest");
+                headers.set("User-Agent", "Mozilla/5.0 (Linux; Android 7.1.1; OD103 Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044203 Mobile Safari/537.36 MicroMessenger/6.6.6.1300(0x26060632) NetType/WIFI Language/zh_CN");
+        		String result = HttpClient.clientHeader(url, HttpMethod.GET, null, headers);
+        		System.out.println("result="+result);
+        		JSONObject obj=JSON.parseObject(result);
+        		String msg = obj.get("msg").toString();
+        		System.out.println("msg="+msg);
+    			int random=(int) (Math.random()*1000+2000);
+    			System.out.println("随机时间："+random);
+    			try {
+					Thread.sleep(random);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+        	}
+        	System.out.println("当前线程："+Thread.currentThread().getName()+",end");
+        }
+        
+       
 	}
 	
 	public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
